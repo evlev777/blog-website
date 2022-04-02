04a10b193417e5d56642d76d4e639fe68c8b7a0d
evlev777 <levkovich.evgeniy.98@gmail.com>
2022-04-02 03:49:05 +0300

    last commit

---
+++ public/js/firebase.js
@@ -0,0 +1,16 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBNhAn5Oog1JfqDIuek-6I6PalrsbiR9j4",
+    authDomain: "blog-website-a76e9.firebaseapp.com",
+    projectId: "blog-website-a76e9",
+    storageBucket: "blog-website-a76e9.appspot.com",
+    messagingSenderId: "570231080521",
+    appId: "1:570231080521:web:fd01a0e66ebe6c9e96e198"
+};
+
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+
